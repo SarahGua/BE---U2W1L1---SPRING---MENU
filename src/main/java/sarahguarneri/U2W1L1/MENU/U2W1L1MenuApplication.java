1079b6fdbd859140c47a4437769865ce905a8930
SarahGuarneri <sarahguarneri97@icloud.com>
2024-01-09 15:20:41 +0100

    FILE

---
+++ src/main/java/sarahguarneri/U2W1L1/MENU/U2W1L1MenuApplication.java
@@ -0,0 +1,13 @@
+package sarahguarneri.U2W1L1.MENU;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2W1L1MenuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2W1L1MenuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sarahguarneri/U2W1L1/MENU/U2W1L1MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package sarahguarneri.U2W1L1.MENU;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2W1L1MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
